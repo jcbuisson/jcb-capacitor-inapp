722ad002f7aa0af498dbc8017c1276635ecc34ab
JC BUISSON <buisson@enseeiht.fr>
2024-09-16 10:55:07 +0200

    WIP

--- ios/Sources/InAppPurchasePlugin/InAppPurchase.swift
+++ ios/Sources/InAppPurchasePlugin/InAppPurchase.swift
@@ -14,8 +14,7 @@ import StoreKit
         async {
             do {
                 if #available(iOS 15.0, *) {
-                    // let products = try await Product.products(for: ["premium"])
-                    let products = try await Product.products(for: [value])
+                    let products = try await Product.products(for: ["premium"])
                     print("products")
                     print(products)
                 } else {
@@ -27,8 +26,50 @@ import StoreKit
                 print(error)
             }
         }
-        return value
+        return "wxc"
     }
+
+
+
+
+
+    // public func buyProduct(_ product: Product) -> String {
+    //     async {
+    //         if #available(iOS 15.0, *) {
+    //             do {
+    //                 let result = try await product.purchase()
+                    
+    //                 switch result {
+    //                 case let .success(.verified(transaction)):
+    //                     // Successful purhcase
+    //                     await transaction.finish()
+    //                 case let .success(.unverified(_, error)):
+    //                     // Successful purchase but transaction/receipt can't be verified
+    //                     // Could be a jailbroken phone
+    //                     print("Unverified purchase. Might be jailbroken. Error: \(error)")
+    //                     break
+    //                 case .pending:
+    //                     // Transaction waiting on SCA (Strong Customer Authentication) or
+    //                     // approval from Ask to Buy
+    //                     break
+    //                 case .userCancelled:
+    //                     // ^^^
+    //                     print("User Cancelled!")
+    //                     break
+    //                 @unknown default:
+    //                     print("Failed to purchase the product!")
+    //                     break
+    //                 }
+    //             } catch {
+    //                 print("Failed to purchase the product!")
+    //             }
+    //         }
+    //     } else {
+    //         // Fallback on earlier versions
+    //         print("needs iOS15")
+    //     }
+    //     return "buy"
+    // }
 
     public func refreshPurchasedProducts() async {
         // Iterate through the user's purchased products.
