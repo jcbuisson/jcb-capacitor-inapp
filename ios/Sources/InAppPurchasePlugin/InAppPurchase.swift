85f5c2c51a33d36b612d1da212799befdab85cd7
JC BUISSON <buisson@enseeiht.fr>
2024-09-16 07:05:06 +0200

    WIP

--- ios/Sources/InAppPurchasePlugin/InAppPurchase.swift
+++ ios/Sources/InAppPurchasePlugin/InAppPurchase.swift
@@ -1,8 +1,53 @@
 import Foundation
+import StoreKit
+
 
 @objc public class InAppPurchase: NSObject {
+
     @objc public func echo(_ value: String) -> String {
+
+        async {
+            do {
+                if #available(iOS 15.0, *) {
+                    let products = try await Product.products(for: ["premium"])
+                    print("products")
+                    print(products)
+                } else {
+                    // Fallback on earlier versions
+                    print("needs iOS15")
+                }
+            }
+            catch {
+                print(error)
+            }
+        }
+
         print(value)
+        print("qsd")
         return value
     }
+
+    @objc public func fetchProducts(_ value: String) -> String {
+
+        async {
+            do {
+                if #available(iOS 15.0, *) {
+                    let products = try await Product.products(for: ["premium"])
+                    print("products")
+                    print(products)
+                } else {
+                    // Fallback on earlier versions
+                    print("needs iOS15")
+                }
+            }
+            catch {
+                print(error)
+            }
+        }
+
+        print(value)
+        print("qsd")
+        return value
+    }
+
 }
