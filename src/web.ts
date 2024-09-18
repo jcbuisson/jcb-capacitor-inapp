1c47fc2a15b8aa54979dc61eecff15e7d36b970c
JC BUISSON <buisson@enseeiht.fr>
2024-09-18 10:00:32 +0200

    WIP

--- src/web.ts
+++ src/web.ts
@@ -3,8 +3,29 @@ import { WebPlugin } from '@capacitor/core';
 import type { InAppPurchasePlugin } from './definitions';
 
 export class InAppPurchaseWeb extends WebPlugin implements InAppPurchasePlugin {
-  async echo(options: { value: string }): Promise<{ value: string }> {
-    console.log('ECHO', options);
-    return options;
-  }
+
+   async echo(options: { value: string }): Promise<{ value: string }> {
+      console.log('ECHO', options)
+      return options
+   }
+
+   async buyProduct(options: { productId: string }): Promise<{ value: string }> {
+      console.log('BUYPRODUCT', options)
+      return {
+         value: "ok"
+      }
+   }
+
+   async getPurchases(options: { value: string }): Promise<{ value: string }> {
+      console.log('GETPURCHASES', options)
+      return {
+         value: "ok"
+      }
+   }
+
+   async test(options: { value: string }): Promise<{ value: string }> {
+      console.log('TEST', options)
+      return options
+   }
+
 }
